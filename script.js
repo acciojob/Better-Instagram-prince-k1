55349f08095d5c0f3a155003b5ebba4519832d60
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-28 16:26:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your code here
+const pic = document.querySelectorAll('.image');
+
+let obj = {
+	dragElement: null,
+	dropElement: null
+}
+pic.forEach(elem => {
+	elem.addEventListener('dragstart', onDragstart);
+	elem.addEventListener('dragover', onDragOver);
+	elem.addEventListener('drop', onDrop);
+})
+
+function onDragstart(event){
+	obj.dragElement = event.target;
+}
+function onDragOver(event){
+	event.preventDefault();
+}
+function onDrop(event){
+	obj.dropElement = event.target;
+	let drop = event.target.id;
+	event.target.id = obj.dragElement.id;
+	obj.dragElement.id = drop;
+}
